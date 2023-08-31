c3ecb030167528ba0311814bbcc464a0bf5ba65b
jiink <jacob.j.carlson03@gmail.com>
2023-08-31 16:36:22 -0700

    Added simple function

--- README.md
+++ README.md
@@ -4,6 +4,12 @@ This is our compiler project for CPSC 440-03 of Fall 2023 with Choi. By the end 
 
 It's called the Peanut Compiler because both rats and gophers can eat peanuts.
 
-### Current state:
+### Current state
 
 Assignment 1 - create a lexer
+
+### Quick start
+
+### Resources
+
+Learning Go: https://go.dev/tour/list
--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -26,10 +26,41 @@ var separators = [...]string{
 	";",
 }
 
+// Returns true if the given string is found in the
+// list of keywords Rat23F recognizes.
+func isKeyword(str string) bool {
+	for _, keyword := range keywords {
+		if str == keyword {
+			return true
+		}
+	}
+	return false
+}
+
+func isOperator(str string) bool {
+	for _, operator := range operators {
+		if str == operator {
+			return true
+		}
+	}
+	return false
+}
+
+func isSeparator(str string) bool {
+	for _, separator := range separators {
+		if str == separator {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
 	fmt.Println("Welcome to the Peanut Lexer!")
 	fmt.Println("Here are the keywords, operators, and separators:")
 	fmt.Println(keywords)
 	fmt.Println(operators)
 	fmt.Println(separators)
+	fmt.Println("Is 'function' a keyword?", isKeyword("function"))
+	fmt.Println("Is 'pineapple' a keyword?", isKeyword("pineapple"))
 }
