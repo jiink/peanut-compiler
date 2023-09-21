6afdc6b02882e6a6144152b0e0ffb8b6dfa50cd7
jiink <jacob.j.carlson03@gmail.com>
2023-09-20 18:48:12 -0700

    Seeing some success with dfsmIdentifier

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"slices"
 )
 
 type tokenType int
@@ -111,10 +112,32 @@ func isDigit(r rune) bool {
 	return r >= '0' && r <= '9'
 }
 
+func charToSymbolType(r rune) symbolType {
+	if isLetter(r) {
+		return Letter
+	}
+	if isDigit(r) {
+		return Digit
+	}
+	return Special
+}
+
 func readCharSourceCode(index *int) rune {
 	char := []rune(sourceCode)[*index]
 	*index = *index + 1
 	return char
+}
+
+func backUp(index *int) bool {
+	if *index < 1 {
+		return false
+	}
+	*index = *index - 1
+	return true
+}
+
+func isAcceptingState(currentState int, acceptingStates []int) bool {
+	return slices.Contains(acceptingStates, currentState)
 }
 
 func check(e error) {
@@ -123,15 +146,46 @@ func check(e error) {
 	}
 }
 
-func dfsmInteger(sourceCodePointer *int, currentChar rune) {
-	inputSymbolSet := []symbolType{Digit, Letter, Special}
+func dfsmIdentifier(sourceCodePointer *int, currentChar rune) bool {
+	inputSymbolSet := []symbolType{Letter, Digit}
+	// Made from making a graph from a regular expression and
+	// converting it to a DFSM by hand, then assigning a number
+	// to each unique state.
 	transitionTable := [][]int{
-		// d  l  s
-		{1, 2, 3}, // 0
-		{1, 4, 5}, // 1
-		{5, 5, 5}, // 2
+		// l  d
+		{0, 0}, // 0
+		{2, 0}, // 1
+		{3, 4}, // 2
+		{3, 4}, // 3
+		{3, 4}, // 4
 	}
-	acceptingStates := []int{1}
+	acceptingStates := []int{2, 3, 4}
+	currentState := 1
+
+	// Back up char pointer
+	backUp(sourceCodePointer)
+
+	maxSteps := 100
+	for i := 0; i < maxSteps; i++ {
+		newChar := readCharSourceCode(sourceCodePointer)
+		fmt.Printf("new char: %c\n", newChar)
+		symbol := charToSymbolType(newChar)
+		columnIndex := slices.Index(inputSymbolSet, symbol)
+		if columnIndex == -1 {
+			fmt.Printf("Invalid symbol: %d\n", symbol)
+			break
+		}
+		fmt.Printf("Current state: %d, Symbol: %d\n", currentState, symbol)
+		currentState = transitionTable[currentState][columnIndex]
+		fmt.Printf("New state: %d\n\n", currentState)
+		// 0 is the unrecoverable state.
+		if currentState == 0 {
+			fmt.Printf("Fell into unrecoverable state.\n")
+			return false
+		}
+	}
+	fmt.Printf("Final state: %d\n", currentState)
+	return isAcceptingState(currentState, acceptingStates)
 }
 
 func main() {
@@ -155,17 +209,27 @@ func main() {
 	fmt.Println("Let the main lexing loop begin...")
 	sourceCodePointer := 0 // Points to the current character in the source code
 	for sourceCodePointer < len(sourceCode) {
+		accepted := false
+		lexemeStartIndex := sourceCodePointer
 		currentChar := readCharSourceCode(&sourceCodePointer)
-		fmt.Printf("Current character: %c\n", currentChar)
+		//fmt.Printf("Current character: %c is type %d\n", currentChar, charToSymbolType(currentChar))
 		if isLetter(currentChar) {
 			//fmt.Println("It's a letter")
 			// Call relevant DFSM
+			accepted = dfsmIdentifier(&sourceCodePointer, currentChar)
 		} else if isDigit(currentChar) {
 			//fmt.Println("It's a digit")
 			// Call relevant DFSM
-			dfsmInteger(&sourceCodePointer, currentChar)
+			//dfsmInteger(&sourceCodePointer, currentChar)
 		} else {
 			fmt.Printf("Unrecognized character %c\n", currentChar)
+		}
+		if accepted {
+			backUp(&sourceCodePointer)
+			lexemeEndIndex := sourceCodePointer
+			fmt.Printf("\"%s\" Accepted! (from %d to %d)\n", sourceCode[lexemeStartIndex:lexemeEndIndex], lexemeStartIndex, lexemeEndIndex)
+		} else {
+			fmt.Printf("Rejected.\n")
 		}
 	}
 }
